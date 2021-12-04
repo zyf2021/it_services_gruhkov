f354290a37e2051e438e42e59982f38255500ea6
Zyf <freelance2020@bk.ru>
2021-12-04 12:39:43 +0300

    Bootsrap

--- app.js
+++ app.js
@@ -7,22 +7,29 @@ const db = require("./models")
 
 const app = express()
 const port = config.get('PORT')||8080
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/',function (req, res) {
-  res.render('register')
-  });
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"))
+})
+app.get("/profile",(req,res)=>{
+  res.sendFile(path.join(__dirname, "views/page2.html"))
+})
+app.get("/order",(req,res)=>{
+  res.sendFile(path.join(__dirname, "views/page3.html"))
+})
+
 app.use(express.json({extended:true}))
 
 app.use('/api/auth', require('./routes/auth.routes.js'))
 app.use('/api/user', require('./routes/user.routes'))
 app.use('/api/request', require('./routes/request.routes'))
 
-//app.use('/', express.static(path.join(__dirname, '..', directoryToServe)))
 
-/*app.get('/', (req,res)=>{
-    res.send('HTTPS!!!!')
-})*/
+
 
 const httpsOptions = {
     cert: fs.readFileSync('server.cert'),
